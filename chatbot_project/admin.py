c3aacc6e80448c0d39e61de6a0c25819a33f249d
Pranav Dalvi <pranavdalvi627@gmail.com>
2022-04-02 15:31:54 +0530

    initial commit

--- chatbot_project/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-
-from chatbot_project.models import Response
-
-# Register your models here.
-admin.site.register(Response)
-# admin.site.register(Topic)
--- chatbot_project/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class ChatbotProjectConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'chatbot_project'
